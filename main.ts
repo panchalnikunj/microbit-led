af40d229c0477ccfc73b150a6efb35b0c825a8a4
Nikunj Panchal <59598242+panchalnikunj@users.noreply.github.com>
2025-02-26 20:06:29 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 namespace screenMagic {
+    /**
+     * Address LEDs liners
+     * Min 0 Max 25
+     */
     export function plotAt(index : number): void{
         index |= 0
         const y = Math.floor(index / 5);
