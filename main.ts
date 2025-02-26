46d35cf4a7cae603fc9211c67c992b834866956e
Nikunj Panchal <59598242+panchalnikunj@users.noreply.github.com>
2025-02-26 20:19:50 +0530

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,7 @@
-namespace screenMagic {
-    /**
-     * Address LEDs linearly row first
-     */
-    //% blockId=screenmagicplotat
-    //% block="plot at $index"
-    //% index.min=0 index.max=25
-    export function plotAt(index : number): void{
-        index |= 0
-        const y = Math.floor(index / 5);
-        const x = Math.floor(index % 5);
-        led.plot(x,y)
+//% weight=100 color=#0fbc11 icon=""
+namespace MyExtension {
+    //% block
+    export function blinkLED(): void {
+        basic.showIcon(IconNames.Heart)
     }
 }
--- test.ts
+++
@@ -1,4 +0,0 @@
-for(let i = 0; i < 25; ++i){
-    screenMagic.plotAt(i)
-    basic.pause(500)
-}
